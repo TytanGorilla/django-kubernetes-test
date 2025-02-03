89faea688ef84b3dc5dcc4becb76f7136041e751
TytanGorilla <65769501+TytanGorilla@users.noreply.github.com>
2025-02-03 18:41:16 +0200

    Fixed pathing by removing leading static folder

--- apps/scheduler/views.py
+++ apps/scheduler/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 def index(request):
     manifest = settings.REACT_MANIFEST.copy()
     files = manifest.get("files", {})
-    # Create new keys that don't contain dots.
-    manifest["files"]["main_js"] = files.get("main.js")
-    manifest["files"]["main_css"] = files.get("main.css")
+    # Create new keys without the leading "/static/"
+    manifest["files"]["main_js"] = files.get("main.js", "").lstrip("/static/")
+    manifest["files"]["main_css"] = files.get("main.css", "").lstrip("/static/")
     return render(request, 'scheduler/scheduler_index.html', {"react_manifest": manifest}) # Points to apps/scheduler/templates/scheduler/index.html
