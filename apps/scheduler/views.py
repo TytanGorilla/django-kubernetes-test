85838dd05c702849a9d364b1f78b4d5f9ee54eab
TytanGorilla <65769501+TytanGorilla@users.noreply.github.com>
2025-02-03 15:27:49 +0200

    Correcting file paths for correctly serving static files

--- apps/scheduler/views.py
+++ apps/scheduler/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.conf import settings
 
 def index(request):
-    return render(request, 'scheduler/scheduler_index.html', {"react_manifest": settings.REACT_MANIFEST}) # Points to apps/scheduler/templates/scheduler/index.html
+    manifest = settings.REACT_MANIFEST.copy()
+    files = manifest.get("files", {})
+    # Create new keys that don't contain dots.
+    manifest["files"]["main_js"] = files.get("main.js")
+    manifest["files"]["main_css"] = files.get("main.css")
+    return render(request, 'scheduler/scheduler_index.html', {"react_manifest": manifest}) # Points to apps/scheduler/templates/scheduler/index.html
