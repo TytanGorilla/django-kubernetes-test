ef38c44bc1b6de1b7a36709e4a7a4abf35a7b875
TytanGorilla <65769501+TytanGorilla@users.noreply.github.com>
2025-02-06 14:20:30 +0200

    Removed additional static that was being appended by my template

--- apps/scheduler/views.py
+++ apps/scheduler/views.py
@@ -11,8 +11,8 @@ def index(request):
     manifest = settings.REACT_MANIFEST.copy()
     files = manifest.get("files", {})
     # Create new keys without the leading "/static/"
-    manifest["files"]["main_js"] = files.get("main.js", "")
-    manifest["files"]["main_css"] = files.get("main.css", "")
+    manifest["files"]["main_js"] = files.get("main.js", "").replace("/static/", "", 1)
+    manifest["files"]["main_css"] = files.get("main.css", "").replace("/static/", "", 1)
     return render(request, 'scheduler/scheduler_index.html', {"react_manifest": manifest}) # Points to apps/scheduler/templates/scheduler/index.html
 
 class EventViewSet(viewsets.ModelViewSet):
