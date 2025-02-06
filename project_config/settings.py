281ece2dd2bd046bc13a62081a124ea769d38f28
TytanGorilla <65769501+TytanGorilla@users.noreply.github.com>
2025-02-06 09:29:21 +0200

    Corrected Django & Nginx staticfiles pathing

--- project_config/settings.py
+++ project_config/settings.py
@@ -152,7 +152,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 STATICFILES_DIRS = [
     BASE_DIR / 'project_config/static',  # Global static files
     BASE_DIR / 'apps/scheduler/static',  # Scheduler app's static files
-    os.path.join(BASE_DIR, "frontend/build/static"),  # ✅ Point Django to React’s build
+    os.path.join(BASE_DIR, "staticfiles/frontend"),  # ✅ Point Django to React’s build
     os.path.join(BASE_DIR, "project_config/static/core"),  # Ensure core CSS is included
 ]
 
