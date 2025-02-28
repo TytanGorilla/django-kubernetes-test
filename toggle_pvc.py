e55c1619142093287e32a9df3f335f056c785ca5
TytanGorilla <65769501+TytanGorilla@users.noreply.github.com>
2025-02-28 12:51:59 +0200

    Adjusted toggle_pvc to correctly prepare pvc manifest according to setup environment

--- toggle_pvc.py
+++ toggle_pvc.py
@@ -7,19 +7,21 @@ def update_file(filepath, env):
     with open(filepath, 'r') as f:
         data = yaml.safe_load(f)
     
-    # Update storageClassName in spec.
+    # Update storageClassName in spec for both PVCs.
     if 'spec' in data:
         if env == "local":
             data['spec']['storageClassName'] = "hostpath"
         elif env == "codespaces":
             data['spec']['storageClassName'] = "standard"
     
-    # For staticfiles-pvc.yaml, update accessModes if present.
-    if 'spec' in data and 'accessModes' in data['spec']:
+    # For staticfiles-pvc.yaml, update accessModes accordingly.
+    basename = os.path.basename(filepath)
+    if basename == "staticfiles-pvc.yaml":
         if env == "local":
-            data['spec']['accessModes'] = ["ReadWriteOnce"]
-        elif env == "codespaces":
             data['spec']['accessModes'] = ["ReadWriteMany"]
+        elif env == "codespaces":
+            data['spec']['accessModes'] = ["ReadWriteOnce"]
+    # For postgres-pvc.yaml, leave accessModes unchanged.
     
     with open(filepath, 'w') as f:
         yaml.safe_dump(data, f, sort_keys=False)
